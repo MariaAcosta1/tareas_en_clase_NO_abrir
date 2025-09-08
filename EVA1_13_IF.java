74aebb9fba4eb139196bbe610c82d2929877d7f3
MariaAcosta1 <L25550705@chihuahua2.tecnm.mx>
2025-09-08 17:56:50 -0600

    Add files via upload

---
+++ EVA1_13_IF.java
@@ -0,0 +1,23 @@
+
+package eva1_13_if;
+import java.util.Scanner;
+public class EVA1_13_IF {
+
+    public static void main(String[] args) {
+        //DECLARACIÓN DE VARIABLES
+        double califa;
+        Scanner captu = new Scanner(System.in);
+        //CAPTURA
+        System.out.println("Introduce la calificación: ");
+        califa = captu.nextDouble();
+        //ESTRUCTURA DE CONTROL IF-THEN-ELSE
+        //SOLO EVALUA VERDADERO (true) o FALSO (false)
+        if (califa >= 70) {
+            //AQUI VA LO QUE CORRESPONDE A VERDADERO
+            System.out.println("Felicitaciones, acreditaste!!");
+        } else {      //SECCIÓN FALSO (opcional)
+            System.out.pritln("Azotes!!");
+        }
+    }
+    
+}
